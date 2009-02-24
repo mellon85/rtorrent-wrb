855c357d2d71b8120ce1115ba96ec296a8d67f83
Dario Meloni <mellon85@gmail.com>
2009-02-24 12:14:23 +0100

    Aggiunta la pagina per i torrents funzionante

--- rtorrent-wrb/controller/main.rb
+++ rtorrent-wrb/controller/main.rb
@@ -11,10 +11,4 @@ class MainController < Controller
     @title = "Welcome to Ramaze!"
   end
 
-  # the string returned at the end of the function is used as the html body
-  # if there is no template for the action. if there is a template, the string
-  # is silently ignored
-  #def notemplate
-  #  "there is no 'notemplate.xhtml' associated with this action"
-  #end
 end
--- rtorrent-wrb/controller/torrent.rb
+++ rtorrent-wrb/controller/torrent.rb
@@ -1,6 +1,7 @@
 class TorrentController < Controller
   def index
     @title = "rtorrent: torrents"
+    @torrents = Torrent.all
   end
 
 end
