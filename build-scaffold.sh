5ea4f84390eb5ae3c7b3a525a610ed8d7d932f00
Dario Meloni <mellon85@gmail.com>
2009-02-24 19:01:53 +0100

    Rimosso scaffold. Aggiunto il nome ai torrent e dati completi

--- build-scaffold.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# License: GPLv3 or newer
-
-ruby script/generate scaffold Torrent name:string size:integer upped:integer downloaded:integer up_rate:integer down_rate:integer torrent_hash:string status:integer
-
-ruby script/generate scaffold Filelist torrent_hash:string name:string size:integer downloaded:integer
-
-ruby script/generate scaffold Tracker torrent_hash:string url:string
--- rtorrent-wrb/model/init.rb
+++ rtorrent-wrb/model/init.rb
@@ -9,8 +9,14 @@ require 'model/models'
 require 'model/migration'
 
 torrents = DB[:torrents]
-torrents << {:torrent_id=>"hash1"}
-torrents << {:torrent_id=>"hash2"}
+torrents << {:torrent_id=>"964D6922206AEE2D2BFB49D8B7CD4C0336EBA311",
+             :name => "[Xvid - ITA- Sub Ita - AC3] Gattaca.avi",
+             :size => 901*1024, :upped => 990*1024, :up => 3,
+             :down => 0, :stat => 0}
+torrents << {:torrent_id=>"AEB3239BC8FB27080FFB9C8696CFB236B9159991",
+             :name => "Starship.Troopers.3.Marauder.(2008)DVDRip.XviD",
+             :size => 1400*1024, :upped => 311*1024, :up => 3,
+             :down => 0, :stat => 2}
 to = Torrent["hash1"]
 
 #ta = Tracker.new
