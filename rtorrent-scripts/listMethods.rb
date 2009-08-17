52b722000d33d60212e0729912930d9820695508
Alessandro Piccioli <alkawiz@gmail.com>
2009-08-17 11:45:59 +0200

    Show "Done!" in main page for complete torrents.

--- rtorrent-scripts/listMethods.rb
+++ rtorrent-scripts/listMethods.rb
@@ -6,8 +6,8 @@ SOCKET="/tmp/rtorrent.sock"
 
 rtorrent = SCGIXMLClient.new([SOCKET,"/RPC2"])
 puts rtorrent.call("system.listMethods")
+puts rtorrent.call("get_down_rate")
+puts rtorrent.call("get_down_total")
 
-torrents = []
-torrents = rtorrent.call("d.multicall","","d.get_base_path=")
-pp torrents
-rtorrent.call("set_download_rate", "10")
+#pp rtorrent.call("d.multicall","","d.get_base_path=")
+#pp rtorrent.call("f.multicall","BD96AA4C228655083AEB3A9E7FC290EBFAC33226","","f.get_path_components=")
